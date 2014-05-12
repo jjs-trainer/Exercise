501117fc780de59aa13fa5bf7ffab29de332cf51
Takashi Maeda <jjs-trainer@outlook.jp>
2014-05-12 15:50:08 +0900

    Add Exercise1

---
+++ src/jp/co/jjs/java_seminar/exercise_20140512_1/Exercise1.java
@@ -0,0 +1,24 @@
+package jp.co.jjs.java_seminar.exercise_20140512_1;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        byte b       = 1;
+        short s      = 10;
+        int i        = 100;
+        long l       = 10000L;
+        boolean bool = false;
+        float f      = 10.0f;
+        double d     = 10.0;
+        char c       = 'a';
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(bool);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+    }
+}
